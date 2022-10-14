c030094b3b87ed15ddda178499e96bb2ac8ac3f6
Jay Lawrence <dimaanojaylawrence@gmail.com>
2022-10-14 23:44:59 +0800

    About section

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,11 @@ type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 1.5 }}
+      className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
 
         <motion.img 
@@ -22,8 +26,20 @@ const About = (props: Props) => {
             alt=''
             className='mb-20 md:mb-0 flex-shrink-0 w-56 rounded-ful object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'/>
 
+        <div className='space-y-10 px-0 md:px-10'>
+          <h4 className='text-4xl font-semibold'>
+            Here is a {" "}
+            <span className='underline decoration-[#F7AB0A]/50'>little</span>{" "}
+            background
+          </h4>
 
-    </div>
+          <p className='text-sm'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime est harum excepturi ullam vitae eveniet, corporis doloremque error quasi modi 
+            aut incidunt perspiciatis? Non officia omnis quae itaque vel vitae.
+          </p>
+        </div>
+
+    </motion.div>
   )
 }
 
